70890a140232e519351867c9b9f5bea3100291e8
CGSchaefer-hub <C.schaefer@mail.siebold-gymnasium.de>
2026-06-21 15:42:32 +0200

    Add input fields and update course post metadata
    Added input fields for title, content, and max participants in frontend-kursleiter.php. Updated the course post and its metadata accordingly.

--- includes/frontend-kursleiter.php
+++ includes/frontend-kursleiter.php
@@ -9,3 +9,27 @@ $user->set_role('kursleiter');
 
 wp_set_current_user($user_id);
 wp_set_auth_cookie($user_id);
+
+<input type="text"
+name="title"
+value="<?php echo esc_attr($course->post_title); ?>">
+
+<textarea name="content">
+<?php echo esc_textarea($course->post_content); ?>
+</textarea>
+
+<input type="number"
+name="max_participants"
+value="<?php echo $max; ?>">
+
+wp_update_post([
+    'ID' => $course_id,
+    'post_title' => $title,
+    'post_content' => $content
+]);
+
+update_post_meta(
+    $course_id,
+    '_cm_max_participants',
+    $max
+);
